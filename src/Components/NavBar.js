cfa852bf60e91173af7af8597ea33c9fdc65ca0b
JVolkK <josiasfigueroabidaurre@gmail.com>
2024-07-07 01:06:58 -0300

    Item Navbar alineados

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,31 +2,31 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "../Styles/Navbar.css";
+import "../Styles/Navbar.css"; // Asegúrate de que esta ruta sea correcta
 
 const NavBar = () => {
   return (
-    <Navbar bg="primary" expand="lg" className="p-0 fixed-top custom-navbar">
+    <Navbar expand="lg" className="p-0 fixed-top navbar-custom">
       <Container fluid>
         <Navbar.Brand as={Link} to="/">
-          Petsion
+          Rocevelt Galery
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarSupportedContent" />
         <Navbar.Collapse id="navbarSupportedContent">
-          <Nav className="ms-auto mb-2 mb-lg-0">
+          <Nav className="ms-auto mb-2 m-2">
             <Nav.Item>
               <Nav.Link as={Link} to="/home">
-                Home
+                Galeria
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
               <Nav.Link as={Link} to="/services">
-                Services
+                Quién soy
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
               <Nav.Link as={Link} to="/contact">
-                Contact
+                Contacto
               </Nav.Link>
             </Nav.Item>
           </Nav>
